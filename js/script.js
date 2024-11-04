862fdf83b2223afdf81743682da9e529393da13a
hugotallys <hgtllys@gmail.com>
2024-11-03 21:56:12 -0300

    ✨ feat: Add calculator interactivity

--- js/script.js
+++ js/script.js
@@ -2,6 +2,46 @@ let firstNumber = '1';
 let secondNumber = '1';
 let operation = '+';
 
+let displayNumber = '0';
+
+const digitButtons = document.querySelectorAll(".btn-digit");
+const display = document.querySelector(".display");
+
+[...digitButtons].forEach(digitButton => {
+    digitButton.addEventListener("click", (e) => {
+        if (display.innerText === '0')
+            display.innerText = '';
+        display.innerText += e.target.innerText;
+    });
+});
+
+const operationButtons = document.querySelectorAll(".btn-operator");
+
+[...operationButtons].forEach(operationButton => {
+    operationButton.addEventListener("click", (e) => {
+        firstNumber = display.innerText;
+        display.innerText = '0';
+        operation = e.target.innerText;
+    });
+});
+
+const equalsButton = document.querySelector(".equals");
+
+equalsButton.addEventListener("click", () => {
+    secondNumber = display.innerText;
+    let result = operate(firstNumber, secondNumber, operation);
+    display.innerText = `${result}`;
+});
+
+const clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", () => {
+    firstNumber = null;
+    secondNumber = null;
+    operation = null;
+    display.innerText = 0;
+});
+
 function add(a, b) {
     return a + b;
 }
@@ -27,9 +67,9 @@ function operate(firstNumber, secondNumber, operation) {
             return add(firstNumber, secondNumber);
         case '-':
             return subtract(firstNumber, secondNumber);
-        case '*':
+        case 'x':
             return multiply(firstNumber, secondNumber);
-        case '/':
+        case '÷':
             return divide(firstNumber, secondNumber);
     }
 }
