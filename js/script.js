40b828fb644f44c6d2be469bee988577e9af3554
hugotallys <hgtllys@gmail.com>
2024-11-03 21:25:22 -0300

    ✨ feat: Implement basic calculator functions and interface

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let firstNumber = '1';
+let secondNumber = '1';
+let operation = '+';
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(firstNumber, secondNumber, operation) {
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+
+    switch (operation) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
+}
